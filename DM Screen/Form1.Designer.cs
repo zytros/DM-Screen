180d839ede51d59a5115b87a8c47d476023862a0
Lucalel <luca.sichi@hotmail.com>
2020-11-25 18:40:36 +0100

    1

--- DM Screen/Form1.Designer.cs
+++ DM Screen/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace DM_Screen
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.lbl_1 = new System.Windows.Forms.Label();
+            this.cmd_addNewCampaign = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -49,11 +50,22 @@ namespace DM_Screen
             this.lbl_1.TabIndex = 1;
             this.lbl_1.Text = "label1";
             // 
+            // cmd_addNewCampaign
+            // 
+            this.cmd_addNewCampaign.Location = new System.Drawing.Point(645, 12);
+            this.cmd_addNewCampaign.Name = "cmd_addNewCampaign";
+            this.cmd_addNewCampaign.Size = new System.Drawing.Size(143, 23);
+            this.cmd_addNewCampaign.TabIndex = 2;
+            this.cmd_addNewCampaign.Text = "Add new Campaign";
+            this.cmd_addNewCampaign.UseVisualStyleBackColor = true;
+            this.cmd_addNewCampaign.Click += new System.EventHandler(this.cmd_addNewCampaign_Click);
+            // 
             // DMScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmd_addNewCampaign);
             this.Controls.Add(this.lbl_1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "DMScreen";
@@ -67,6 +79,7 @@ namespace DM_Screen
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Button cmd_addNewCampaign;
     }
 }
 
--- DM Screen/Form1.cs
+++ DM Screen/Form1.cs
@@ -18,28 +18,30 @@ namespace DM_Screen
         Button[] campaigns;
         public DMScreen()
         {
-            String[] campaign = System.IO.File.ReadAllLines(@"C:\Users\Lucalel\source\repos\DM Screen\DM Screen\res\campaigns\campaigns.txt");
-            NUMBER_OF_CAMPAIGNS = Int32.Parse(campaign[0]);
             InitializeComponent();
-            buttonArray(NUMBER_OF_CAMPAIGNS);
-        }
-
-        private void buttonArray(int n)
-        {
-            for(int i = 0; i < n; i++)
+            String[] campaign = System.IO.File.ReadAllLines(@"res\campaigns\campaigns.txt");
+            NUMBER_OF_CAMPAIGNS = Int32.Parse(campaign[0]);
+            for (int i = 0; i < NUMBER_OF_CAMPAIGNS; i++)
             {
                 Button btn = new Button();
                 btn.Size = new Size(100, 100);
-                btn.Text = i.ToString();
+                btn.Text = campaign[i + 1];
                 btn.Click += cmd_Campaign_Click;
                 flowLayoutPanel1.Controls.Add(btn);
             }
         }
 
+        
+
         private void cmd_Campaign_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
             lbl_1.Text = lbl_1.Text + btn.Text;
+        }
+
+        private void cmd_addNewCampaign_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
